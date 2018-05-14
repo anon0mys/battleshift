cbf441a15d4144d97d720b7dc3657da0f2ba61f9
Evan Wheeler <anon0mys87@gmail.com>
2018-05-14 16:30:56 -0600

    Registration mailer test actually testing things

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,2 +1,5 @@
 class RegistrationMailer < ApplicationMailer
+  def confirmation_email(user)
+    mail(to: user.email, subject: 'Battleshift Account Confirmation')
+  end
 end
--- spec/mailers/registration_mailer_spec.rb
+++ spec/mailers/registration_mailer_spec.rb
@@ -1,16 +1,14 @@
 require "rails_helper"
 
 RSpec.describe RegistrationMailer, type: :mailer do
-  describe 'sends confirmation email' do
-    from = 'me@example.com'
-    to = 'to@example.com'
-    email = RegistrationMailer.confirmation_email(from, to)
+  it 'sends confirmation email' do
+    user = create(:user, email: 'test@mail.com')
+    expected_body = 'Visit here to activate your account.'
 
-    expect(email.deliver_now).to eq(1)
+    email = described_class.confirmation_email(user).deliver_now
 
-    expect(email.from).to eq(from)
-    expect(email.to).to eq(to)
+    expect(email.to).to eq(user.email)
     expect(email.subject).to eq('Battleshift Account Confirmation')
-    expect(email.body.to_s).to eq(read_fixture('confirmation_email').join)
+    expect(email.body.to_s).to eq(expected_body)
   end
 end
