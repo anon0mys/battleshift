a7c6bfe83a03f2ae0748fba00d7fe92e8b55ebf4
VadLusk <AdLusk@gmail.com>
2018-05-14 14:37:27 -0600

    Add skeleton for test.

---
+++ spec/features/user_logs_in_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+feature 'user logs in' do
+  context 'and registers for the first time' do
+    scenario 'and gets to dashboard with not activated message' do
+      
+    end
+  end
+end
