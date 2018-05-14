725a7413c41f70954f580ed3c7821820c4ad30cd
VadLusk <AdLusk@gmail.com>
2018-05-14 15:29:20 -0600

    Add validation of password digest to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates_presence_of :name, :email, :password_digest
+  validates :password_digest, confirmation: {case_sensitive: true}
   has_secure_password
   enum status: ['inactive', 'active']
 
