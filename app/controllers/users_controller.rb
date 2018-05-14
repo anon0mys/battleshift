829b839c9efb309ae0b219c00b0d83768c09c9d2
VadLusk <AdLusk@gmail.com>
2018-05-14 15:12:22 -0600

    Add create method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,21 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Registration successful'
+      redirect_to dashboard_path
+    else
+      flash[:error] = 'Registration unsuccessful'
+      redirect_to register_path
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
