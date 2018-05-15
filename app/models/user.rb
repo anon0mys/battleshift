c6c2fb0cc99ed05fb01bef628d39399b0bd1e69e
VadLusk <AdLusk@gmail.com>
2018-05-14 21:20:34 -0600

    Comment out api key validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
-  validates_presence_of :name, :email, :password_digest
+  validates_presence_of :name, :email, :password_digest, :api_key
+  validates_uniqueness_of :api_key
   validates :password_digest, confirmation: {case_sensitive: true}
   has_secure_password
   enum status: ['inactive', 'active']
 
+  before_validation :set_api_key
+
+  private
+
+    def set_api_key
+      self.api_key = ApiKey.generate
+    end
 end
