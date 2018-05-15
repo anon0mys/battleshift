89ee79dbb0adbc8d9c54c10b818ed47580015514
Evan Wheeler <anon0mys87@gmail.com>
2018-05-15 09:57:30 -0600

    Adds user activation in controller, updates dashboard to have status if user is active

--- app/controllers/activation_controller.rb
+++ app/controllers/activation_controller.rb
@@ -1,20 +1,7 @@
 class ActivationController < ApplicationController
   def update
-    user = User.find(params[:id])
-    if status_params[:token]
-      user.update(status: 'active')
-      redirect_to activation_path(user)
-    else
-      render file: "#{Rails.root}/public/404.html", status: 404
-    end
-  end
-
-  def show
-  end
-
-  private
-
-  def status_params
-    params.require(:authentication).permit(:token)
+    user = User.find(params[:format])
+    user.active!
+    render :activated
   end
 end
