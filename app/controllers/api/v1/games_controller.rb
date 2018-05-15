7aa09d7b285ed84cfaf4cea7e93610833ce5a302
Evan Wheeler <anon0mys87@gmail.com>
2018-05-15 14:27:59 -0600

    Updates game endpoint to create a new battleship game

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -5,6 +5,16 @@ class Api::V1::GamesController < ApiController
   end
 
   def create
-    binding.pry
+    player_1 = Player.new(Board.new(4))
+    player_2 = Player.new(Board.new(4))
+
+    render json: Game.create({
+      player_1_board: player_1.board,
+      player_2_board: player_2.board,
+      winner: nil,
+      player_1_turns: 0,
+      player_2_turns: 0,
+      current_turn: 'challenger'
+      })
   end
 end
