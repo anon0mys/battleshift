978abecbb31fdbb6d4dc14c6ffe219432c2fdee0
Evan Wheeler <anon0mys87@gmail.com>
2018-05-15 09:21:47 -0600

    Adds resuce clause to application controller for record not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def not_found
+    render file: "#{Rails.root}/public/404.html", status: 400
   end
 end
