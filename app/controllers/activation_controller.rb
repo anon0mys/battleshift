b4c57617eae850b68fa450258dae52c9108c19e3
Evan Wheeler <anon0mys87@gmail.com>
2018-05-15 00:54:37 -0600

    Tentative activation update method added to activation controller

--- app/controllers/activation_controller.rb
+++ app/controllers/activation_controller.rb
@@ -1,5 +1,20 @@
 class ActivationController < ApplicationController
   def update
-    binding.pry
+    user = User.find(params[:id])
+    if status_params[:token]
+      user.update(status: 'active')
+      redirect_to activation_path(user)
+    else
+      render file: "#{Rails.root}/public/404.html", status: 404
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def status_params
+    params.require(:authentication).permit(:token)
   end
 end
