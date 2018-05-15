b536f7f176f03c581200ec16a4b5c9e952667f82
VadLusk <AdLusk@gmail.com>
2018-05-14 21:14:06 -0600

    Add migration add api key to users.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :email }
     it { should validate_presence_of :name }
     it { should validate_presence_of :password_digest }
+    it { should validate_presence_of :api_key }
   end
   describe 'callbacks' do
     it 'assigns api key to user upon creation' do
