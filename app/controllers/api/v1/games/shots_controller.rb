1cebffb5ac42be278c13b18301f90843e8d15ef4
Evan Wheeler <anon0mys87@gmail.com>
2018-05-18 11:12:10 -0600

    Refactors player authentication to be part of the api controller

--- app/controllers/api/v1/games/shots_controller.rb
+++ app/controllers/api/v1/games/shots_controller.rb
@@ -1,10 +1,8 @@
 class Api::V1::Games::ShotsController < ApiController
 
   def create
-    @player = User.find_by(api_key: request.headers['X-API-Key'])
-    raise ApiExceptions::InvalidApiKey.new('Unauthorized', 401) if @player.nil?
     @game = Game.find(params[:game_id])
-    turn_processor = TurnProcessor.new(@game, params[:shot][:target], @player)
+    turn_processor = TurnProcessor.new(@game, params[:shot][:target], set_player)
 
     turn_processor.run!
     render json: @game, message: turn_processor.message
