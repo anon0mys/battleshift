4b28e3ff14587fcea65b677288daff75f04b9529
Evan Wheeler <anon0mys87@gmail.com>
2018-05-20 23:22:57 -0600

    Fixes seeds file, moves exceptions from lib to services

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,36 @@
+# Create Users
+user_1 = User.new(name: 'User 1', email: 'user_1@mail.com', password: 'password', status: 'active')
+user_1.set_api_key
+user_1.save!
+user_2 = User.new(name: 'User 2', email: 'user_2@mail.com', password: 'password', status: 'active')
+user_2.set_api_key
+user_2.save!
+
+
+# Create Boards
 player_1_board = Board.new(4)
 player_2_board = Board.new(4)
 
-sm_ship = Ship.new(2)
-md_ship = Ship.new(3)
+# Create Ships
+sm_ship = Ship.new(2, 'A1', 'A2')
+md_ship = Ship.new(3, 'B1', 'D1')
 
 # Place Player 1 ships
-ShipPlacer.new(board: player_1_board,
-               ship: sm_ship,
-               start_space: "A1",
-               end_space: "A2").run
-
-ShipPlacer.new(board: player_1_board,
-               ship: md_ship,
-               start_space: "B1",
-               end_space: "D1").run
+ShipPlacer.new(player_1_board, sm_ship).run
+ShipPlacer.new(player_1_board, md_ship).run
 
 # Place Player 2 ships
-ShipPlacer.new(board: player_2_board,
-               ship: sm_ship.dup,
-               start_space: "A1",
-               end_space: "A2").run
-
-ShipPlacer.new(board: player_2_board,
-               ship: md_ship.dup,
-               start_space: "B1",
-               end_space: "D1").run
+ShipPlacer.new(player_2_board, sm_ship.dup).run
+ShipPlacer.new(player_2_board, md_ship.dup).run
 
 game_attributes = {
+  player_1: user_1,
+  player_2: user_2,
   player_1_board: player_1_board,
   player_2_board: player_2_board,
   player_1_turns: 0,
   player_2_turns: 0,
-  current_turn: "challenger"
+  current_turn: 'player_1'
 }
 
 game = Game.new(game_attributes)
